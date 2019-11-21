f794316276d8341879edac741cd29dde3ecf8070
Karol <karolczak1>
2019-11-21 15:49:20 +0100

    Add new labels for classification dataset

--- classification/dataset.py
+++ classification/dataset.py
@@ -7,7 +7,7 @@ from data_encoding import to_integers, to_binary
 from graph.load_graph import load_graph_from_csv
 
 
-def create_dataset_classification(graph_filename, labels_filename):
+def create_dataset_classification(graph_filename, labels_filenames):
     """
     Create dataset for node classification task.
 
@@ -15,24 +15,30 @@ def create_dataset_classification(graph_filename, labels_filename):
     :param labels_filename: name of CSV file with pairs <node id, label>
     """
     g = load_graph_from_csv(graph_filename)
-    labels_dict = dict(np.loadtxt(labels_filename, skiprows=1, delimiter=";", dtype=np.str))
     nodes = []
     labels = []
 
-    for node in g.nodes:
-        if node in labels_dict:
-            nodes.append([node])
-            labels.append([labels_dict[node]])
+    labels_dict_full = dict()
+
+    for labels_filename in labels_filenames:
+        print(labels_filename)
+        labels_array = np.loadtxt(labels_filename, skiprows=1, delimiter=";", dtype=np.str)
+        labels_dict = dict(labels_array[:, 0:2])
+        labels_dict_full.update(labels_dict)
+        for node in g.nodes:
+            if node in labels_dict:
+                nodes.append([node])
+                labels.append([labels_dict[node]])
 
     print("Encoding to integers")
 
     nodes = to_integers(g.nodes, nodes)
-    labels = to_integers(list(labels_dict.values()), labels)
+    labels = to_integers(list(labels_dict_full.values()), labels)
 
     nodes, labels = shuffle(nodes, labels, random_state=42)
     assert len(nodes) == len(labels)
 
-    print("Saving dataset. Size: %d" % len(nodes))
+    print("Saving dataset. Size: %d. Unique labels: %d" % (len(nodes), len(np.unique(labels))))
 
     pickle.dump((nodes, labels), open(file=graph_filename + "_classification.pickle", mode='wb'))
 
@@ -44,4 +50,5 @@ def load_data(filename):
 
 
 if __name__ == '__main__':
-    create_dataset_classification("../data/dane.csv", "../data/dictionaries/jednostki_chorobowe_icd_grupa.csv")
+    create_dataset_classification("../data/dane.csv", ["../data/dictionaries/jednostki_chorobowe_icd_grupa.csv",
+                                                       "../data/dictionaries/objawy_icd_grupa.csv"])
--- classification/train_and_test.py
+++ classification/train_and_test.py
@@ -8,13 +8,15 @@ from classification.dataset import load_data
 from classification.model import create_model
 
 n_split = 10
-epochs = 100
+epochs = 10
 vocab_size = 2238
 vector_dim = 100
 
 data_file = "../data/dane.csv_classification.pickle"
 
 x_data, y_data = load_data(filename=data_file)
+
+fold_size = x_data.size / n_split
 
 print("Inputs shape %s" % (x_data.shape,))
 print("Outputs shape %s" % (y_data.shape,))
@@ -29,11 +31,14 @@ for algorithm in ["multimetapath2vec", "metapath2vec", "node2vec", "onehot"]:
     load_model = partial(create_model, input_size=vocab_size, hidden_size=vector_dim, output_size=y_data.shape[1],
                          embeddings_file=embeddings_file)
     total_results = []
-    for missing_percentage in range(0, 100, 10):
+    for missing_percentage in range(90, 100, 1):
         print("\t%d%% missing data" % missing_percentage)
         results = []
-        for idx, (train_index, test_index) in enumerate(KFold(n_split, shuffle=True, random_state=42).split(x_data)):
-            print("\t\tFold %d/%d" % (idx + 1, n_split))
+        k_fold = KFold(n_splits=n_split, shuffle=True, random_state=42)
+        fold_no = 0
+        for train_index, test_index in k_fold.split(x_data):
+            fold_no += 1
+            print("\t\tFold %d/%d" % (fold_no, n_split))
             x_train, x_test = x_data[train_index], x_data[test_index]
             y_train, y_test = y_data[train_index], y_data[test_index]
 
@@ -45,14 +50,15 @@ for algorithm in ["multimetapath2vec", "metapath2vec", "node2vec", "onehot"]:
 
             model = load_model()
             model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=["accuracy"])
-            model.fit(x=x_train, y=y_train, validation_data=(x_test, y_test), batch_size=1, epochs=epochs, verbose=0)
+
+            model.fit(x=x_train, y=y_train,  batch_size=1, epochs=epochs, verbose=0)
 
             y_pred = model.predict(x_test)
             y_pred_bool = numpy.argmax(y_pred, axis=1)
             y_test_bool = numpy.argmax(y_test, axis=1)
 
-            f1_micro = f1_score(y_test_bool, y_pred_bool, average='micro', labels=numpy.unique(y_pred_bool))  # accuracy
-            f1_macro = f1_score(y_test_bool, y_pred_bool, average='macro', labels=numpy.unique(y_pred_bool))
+            f1_micro = f1_score(y_test_bool, y_pred_bool, average='micro')  # accuracy
+            f1_macro = f1_score(y_test_bool, y_pred_bool, average='macro')
 
             results.append((f1_micro, f1_macro))
 
